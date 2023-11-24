c4f9d6ce1053490cbaaed47c2822bfbe0bcfe83f
SKWCE2023 <shubham.expert@kirtikumarar.com>
2023-11-24 19:05:28 +0530

    Updates add customer screen

--- assistant/add_client.py
+++ assistant/add_client.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk, messagebox, PhotoImage
 import requests
 from db import create_customer
+from tkcalendar import DateEntry
 
 class AddClientFrame:
     def __init__(self, root):
@@ -21,24 +22,32 @@ class AddClientFrame:
             key = label_text.lower().replace(' ', '_')
             label = ttk.Label(self.frame, text=label_text)
             label.grid(row=i, column=0, padx=10, pady=10, sticky=tk.W)
-            entry = ttk.Entry(self.frame, width = 37, font = ("Helvetica", 10))
-            entry.grid(row=i, column=1, padx=10, pady=10)
             if key == 'date_of_birth':
-                entry.insert(0, 'DD/MM/YYYY')
+                entry = DateEntry(self.frame, selectmode='day', textvariable=key, width = 35, font = ("Helvetica", 10))
+            elif key == 'phone_number':
+                entry = ttk.Entry(self.frame, width = 37, font = ("Helvetica", 10))
+                entry.insert(0, '+{country_code}{number}')
                 entry.configure(foreground="gray")
                 entry.bind("<FocusIn>", self.on_entry_focus_in)
                 entry.bind("<FocusOut>", self.on_entry_focus_out)
+            else:
+                entry = ttk.Entry(self.frame, width = 37, font = ("Helvetica", 10))
+            entry.grid(row=i, column=1, padx=10, pady=10)
             self.entries[key] = entry
         add_button = ttk.Button(self.frame, text="Add Client", command=self.add_client)
         add_button.grid(row=len(labels), column=0, columnspan=2, pady=14)
 
     def add_client(self):
         try:
+            data = {}
             for key, entry in self.entries.items():
-                if not entry.get():
+                if not entry.get() and key != 'company_name':
                     messagebox.showerror("Error", "Please fill in all the fields.")
                     return
-                data[key] = entry.get()
+                if key == 'date_of_birth':
+                    data[key] = entry.get_date().strftime("%d/%m/%Y")
+                else:
+                    data[key] = entry.get()
             response = create_customer(data)
             if response.get("error_message"):
                 messagebox.showerror("Operation Failed", "Failed to add customer. Please try again")
@@ -49,15 +58,15 @@ class AddClientFrame:
             print(f"Error in the request: {e}")
 
     def on_entry_focus_in(self, event):
-        entry = self.entries['date_of_birth']
-        if entry.get() == 'DD/MM/YYYY':
+        entry = self.entries['phone_number']
+        if entry.get() == '+{country_code}{number}':
             entry.delete(0, tk.END)
             entry.configure(show="")
             entry.configure(foreground="black")
 
     def on_entry_focus_out(self, event):
-        entry = self.entries['date_of_birth']
+        entry = self.entries['phone_number']
         if entry.get() == '':
-            entry.insert(0, 'DD/MM/YYYY')
+            entry.insert(0, '+{country_code}{number}')
             entry.configure(show="")
             entry.configure(foreground="gray")
